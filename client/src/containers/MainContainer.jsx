664174d827e05fec050e658d9bf65762a01d8191
Anelise Bergin <anelise.bergin@gmail.com>
2021-12-11 17:43:56 -0700

    working on places

--- client/src/containers/MainContainer.jsx
+++ client/src/containers/MainContainer.jsx
@@ -1,10 +1,15 @@
 import { Switch, Route } from 'react-router-dom';
 import Home from '../screens/Home';
+import Places from '../screens/Places';
 
 export default function MainContainer() {
   return (
     <div>
       <Switch>
+
+      <Route path='/places'>
+          <Places />
+        </Route>
 
         <Route path='/'>
           <Home />
---
+++ client/src/screens/Places.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+export default function Places() {
+  return (
+    <div>
+      <h2>username will be here</h2>
+      <p>places will be here</p>
+
+    </div>
+  )
+}

1987f3a490d0f4dcbadea14eb4fe51995964f5eb
Anelise Bergin <anelise.bergin@gmail.com>
2021-12-15 09:25:24 -0700

    add place and edit place pages styling complete

--- client/src/screens/PlaceEdit.jsx
+++ client/src/screens/PlaceEdit.jsx
@@ -1,3 +1,5 @@
+import '../assets/css/PlaceEdit.css';
+import orangePin from '../assets/graphics/pin-orange.png'
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import lists from '../utils/lists'
@@ -37,68 +39,71 @@ export default function PlaceEdit({ places, handlePlaceUpdate }) {
   };
 
   return (
-    <div>
-      <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handlePlaceUpdate(id, formData);
-      }}
-      >
-        <h3>edit</h3>
-        <label>List:</label>
-        <select name='list' onChange={handleChange} defaultValue='state'>
-        <option disabledvalue='state'>
-            {formData.list}
-          </option>
-          {lists.map((list) => (
-            <option value={list.list} key={list.id} required>
-            {list.list}
-            </option>
-          ))}
-        </select>
-        <br />
-        <br />
-        <label>Country:</label>
-        <select name='country' onChange={handleChange} defaultValue='state'>
+    <div className='edit-place'>
+      <div className='edit-place-container'>
+        <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handlePlaceUpdate(id, formData);
+        }}
+        >
+          <img className='add-pin' src={orangePin}></img>
+          <h2>update this place</h2>
+          <label>List:</label>
+          <select name='list' onChange={handleChange} defaultValue='state'>
           <option disabledvalue='state'>
-            {formData.country}
-          </option>
-          {countries.map((country) => (
-            <option value={country.name} key={country.id} required>
-            {country.name}
+              {formData.list}
             </option>
-          ))}
-        </select>
-        <br />
-        <br />
-        <label>Month:</label>
-        <select name='month' onChange={handleChange} defaultValue='state'>
-        <option disabledvalue='state'>
-            {formData.month}
-          </option>
-          {months.map((month) => (
-            <option value={month.month} key={month.id}>
-            {month.month}
-            </option>
-          ))}
-        </select>
-        <br />
-        <br />
-        <label>Year:</label>
-          <select name='year' onChange={handleChange} defaultValue='state'>
-          <option disabledvalue='state'>
-            {formData.year}
-          </option>
-            {years.map((year) => (
-              <option value={year.year} key={year.id}>
-              {year.year}
+            {lists.map((list) => (
+              <option value={list.list} key={list.id} required>
+              {list.list}
               </option>
             ))}
           </select>
-        <br />
-        <br />
-      <button>update</button>
-      </form>
+          <br />
+          <br />
+          <label>Country:</label>
+          <select name='country' onChange={handleChange} defaultValue='state'>
+            <option disabledvalue='state'>
+              {formData.country}
+            </option>
+            {countries.map((country) => (
+              <option value={country.name} key={country.id} required>
+              {country.name}
+              </option>
+            ))}
+          </select>
+          <br />
+          <br />
+          <label>Month:</label>
+          <select name='month' onChange={handleChange} defaultValue='state'>
+          <option disabledvalue='state'>
+              {formData.month}
+            </option>
+            {months.map((month) => (
+              <option value={month.month} key={month.id}>
+              {month.month}
+              </option>
+            ))}
+          </select>
+          <br />
+          <br />
+          <label>Year:</label>
+            <select name='year' onChange={handleChange} defaultValue='state'>
+            <option disabledvalue='state'>
+              {formData.year}
+            </option>
+              {years.map((year) => (
+                <option value={year.year} key={year.id}>
+                {year.year}
+                </option>
+              ))}
+            </select>
+          <br />
+          <br />
+        <button className='edit-button'>update</button>
+        </form>
+      </div>
     </div>
   )
 }

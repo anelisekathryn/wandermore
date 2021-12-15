9373b8c721033c20d8ee38212692e2b3bb5d60ce
Anelise Bergin <anelise.bergin@gmail.com>
2021-12-15 07:52:02 -0700

    surge deployment 12/15 AM

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username, :email, :password)
+      params.require(:user).permit(:username, :email, :password, :profile_image_url)
     end
 end

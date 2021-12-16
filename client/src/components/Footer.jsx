203e536c6575c2522b568bae11433cab79f14d1e
Anelise Bergin <anelise.bergin@gmail.com>
2021-12-15 19:27:24 -0700

    about page media queries

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,9 +5,9 @@ export default function Footer({places, currentUser, handleSignOut}) {
   return (
     <footer>
       <div className='footer-left'>
-        <p>Site created by Anelise Kathryn</p>
-        <p>See what else I'm working on: GitHub</p>
-        <p>About Wandermore</p>
+        <p>Site created by <a href="https://www.linkedin.com/in/anelisebergin/" target="_blank" rel="noreferrer">Anelise Kathryn</a></p>
+        <p>See what else I'm working on: <a href="https://github.com/anelisekathryn" target="_blank" rel="noreferrer">GitHub</a></p>
+        <p><a>About Wandermore</a></p>
       </div>
       <div className='footer-right'>
         {currentUser?.id === places.user_id ?
